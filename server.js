bf60fd36fafcd01e6915da7b4d7bea361905659e
cojoco <cojoco@users.noreply.github.com>
2015-10-30 00:28:59 -0600

    Got scripture text from API

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var request = require("request")
 var mkdirp = require('mkdirp');
 var app = express();
 var options = {
-    host: '52.88.191.100',
+    host: '127.0.0.1',
     key: fs.readFileSync('ssl/server.key'),
     cert: fs.readFileSync('ssl/server.crt')
 };
 
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(3004);
+http.createServer(app).listen(5000);
+https.createServer(options, app).listen(5001);
 
 app.get('/download-scriptures', function (req, res) {
 
@@ -26,7 +26,6 @@ app.get('/download-scriptures', function (req, res) {
         "dc",
         "pgp"
     ];
-
     var jsonFile;
     testaments.forEach(function(testament) {
         var url = "http://scriptures.desh.es/api/v1/" + testament;
@@ -36,13 +35,13 @@ app.get('/download-scriptures', function (req, res) {
         }, function (error, response, body) {
             if (!error && response.statusCode === 200) {
                 var books = body.books;
-                fs.chmod("scriptures", 0777);
+                fs.chmod("scriptures", 0755);
                 fs.writeFile("scriptures/" + testament + ".json", JSON.stringify(body), function(err) {
                     if(err) {
                         console.log(err);
                     }
                     console.log("Saved: " + testament);
-                }); 
+                });
 
                 books.forEach(function(book) {
                     getBook(testament, book);
@@ -51,7 +50,7 @@ app.get('/download-scriptures', function (req, res) {
             }
         });
     });
-    
+
     res.send(JSON.stringify(jsonFile));
 });
 
@@ -63,14 +62,14 @@ var getBook = function(testament, book) {
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
             mkdirp("scriptures/" + testament, function(err) {console.log(err);});
-            fs.chmod("scriptures/" + testament, 0777);
+            fs.chmod("scriptures/" + testament, 0755, function(err) {if(err) {console.log(err);}});
             fs.writeFile("scriptures/" + testament + "/" + book.slug + ".json", JSON.stringify(body), function(err) {
                 if(err) {
                     console.log(err);
                 }
 
                 console.log("Saved: " + book.slug);
-            }); 
+            });
             getChapter(testament, book, book.chapters);
             jsonFile = body;
         }
@@ -79,27 +78,30 @@ var getBook = function(testament, book) {
 
 
 var getChapter = function(testament, book, chapters) {
-    for(var chapter = 0; chapter < chapters; ++chapter) {
-        var url = "http://scriptures.desh.es/api/v1/" + testament + "/" + book.slug + "/" + chapter;
-        console.log(url);
-        request({
-            url: url,
-            json: true
-        }, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                mkdirp("scriptures/" + testament + "/" + book.slug, function(err) {console.log(err);});
-                fs.chmod("scriptures/" + testament + "/" + book.slug, 0777);
-                fs.writeFile("scriptures/" + testament + "/" + book.slug + "/" + chapter + ".json", JSON.stringify(body), function(err) {
-                    if(err) {
-                        console.log(err);
-                    }
-
-                    console.log("Saved: " + chapter);
-                }); 
-            }
-            else {
-                console.log(error);
-            }
-        });
+    for(var chapter = 1; chapter <= chapters; chapter++) {
+        chapterRequest(testament, book, chapter);
     }
+}
+
+var chapterRequest = function(testament, book, chapter) {
+  var url = "http://scriptures.desh.es/api/v1/" + testament + "/" + book.slug + "/" + chapter;
+  request({
+      url: url,
+      json: true
+  }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+          mkdirp("scriptures/" + testament + "/" + book.slug, function(err) {console.log(err);});
+          fs.chmod("scriptures/" + testament + "/" + book.slug, 0755, function(err) {console.log(err);});
+          fs.writeFile("scriptures/" + testament + "/" + book.slug + "/" + chapter + ".json", JSON.stringify(body), function(err) {
+              if(err) {
+                  console.log(err);
+              }
+
+              console.log("Saved: " + book.title + " " + chapter);
+          });
+      }
+      else {
+          console.log(error);
+      }
+  });
 }
