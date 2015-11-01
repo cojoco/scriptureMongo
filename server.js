95ad7e40502db37c5b99ac00f43b2be240fe69b9
Daniel Hair <danielmhair@gmail.com>
2015-11-01 01:46:32 +0000

    Added missing files and made mongoPopulateWithFile endpoint

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var readline = require('readline');
 var MongoClient = require('mongodb').MongoClient;
 var request = require("request")
 var mkdirp = require('mkdirp');
+var MongoClient = require('mongodb').MongoClient;
 var app = express();
 var options = {
     host: '127.0.0.1',
@@ -16,6 +17,72 @@ var options = {
 
 http.createServer(app).listen(5000);
 https.createServer(options, app).listen(5001);
+
+app.get('/populateMongoWithScriptures', function (req, res) {
+    var testaments = [
+        "ot",
+        "nt",
+        "bm",
+        "dc",
+        "pgp"
+    ];
+    var jsonFile;
+    testaments.forEach(function(testament) {
+        var url = "http://scriptures.desh.es/api/v1/" + testament;
+        fs.readFile("scriptures/" + testament + ".json", 'utf8', function(err, data) {
+            if(err) {
+                throw err;
+            }
+            
+            var books = JSON.parse(data).books;
+            addJSONToMongo(JSON.parse(data));
+            books.forEach(function(book) {
+                bookFileToMongo(testament, book);
+            });
+        });
+    });
+
+    res.send(JSON.stringify(jsonFile));
+});
+
+var bookFileToMongo = function(testament, book) {
+    fs.readFile("scriptures/" + testament + "/" + book.slug + ".json", 'utf8', function(err, data) {
+        if(err) {
+            throw err;
+        }
+
+        addJSONToMongo(JSON.parse(data));
+        chapterFileToMongo(testament, book, book.chapters);
+    });
+}
+
+
+var chapterFileToMongo = function(testament, book, chapters) {
+    for(var chapter = 1; chapter <= chapters; chapter++) {
+        chapterFileToMongoRequest(testament, book, chapter);
+    }
+}
+
+var chapterFileToMongoRequest = function(testament, book, chapter) {
+  fs.readFile("scriptures/" + testament + "/" + book.slug + "/" + chapter + ".json", 'utf8', function(err, data) {
+    if(err) {
+        throw err;
+    }
+
+    addJSONToMongo(JSON.parse(data));
+
+  });
+}
+
+var addJSONToMongo = function(body) {
+    MongoClient.connect("mongodb://localhost/scriptures", function(err, db) {
+      if(err) throw err;
+      db.collection('scriptures').insert(body,function(err, records) {
+        console.log("Record added as "+records[0]._id);
+      });
+    });
+}
+
 
 app.get('/download-scriptures', function (req, res) {
 
@@ -44,7 +111,7 @@ app.get('/download-scriptures', function (req, res) {
                 });
 
                 books.forEach(function(book) {
-                    getBook(testament, book);
+                    saveBookToFile(testament, book);
                 });
 
             }
@@ -54,7 +121,7 @@ app.get('/download-scriptures', function (req, res) {
     res.send(JSON.stringify(jsonFile));
 });
 
-var getBook = function(testament, book) {
+var saveBookToFile = function(testament, book) {
     var url = "http://scriptures.desh.es/api/v1/" + testament + "/" + book.slug;
     request({
         url: url,
@@ -70,20 +137,20 @@ var getBook = function(testament, book) {
 
                 console.log("Saved: " + book.slug);
             });
-            getChapter(testament, book, book.chapters);
+            saveChapterToFile(testament, book, book.chapters);
             jsonFile = body;
         }
     });
 }
 
 
-var getChapter = function(testament, book, chapters) {
+var saveChapterToFile = function(testament, book, chapters) {
     for(var chapter = 1; chapter <= chapters; chapter++) {
-        chapterRequest(testament, book, chapter);
+        chapterFileRequest(testament, book, chapter);
     }
 }
 
-var chapterRequest = function(testament, book, chapter) {
+var chapterFileRequest = function(testament, book, chapter) {
   var url = "http://scriptures.desh.es/api/v1/" + testament + "/" + book.slug + "/" + chapter;
   request({
       url: url,
